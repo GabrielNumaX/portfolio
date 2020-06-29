26749d23f85408de23f36d887dd2e83522cbf6d1
GabrielNumaX <37051017+GabrielNumaX@users.noreply.github>
2020-06-29 17:55:24 -0300

    Updates

---
+++ precache-manifest.d6fa164ef1a8359868c72540b178152c.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d4d2430ac67569fdd1b6aab4bba39aba",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "9d5cd0f10645810cb389",
+    "url": "/portfolio/static/css/main.3bbcb16b.chunk.css"
+  },
+  {
+    "revision": "92d0b0c95759cb8b6d1c",
+    "url": "/portfolio/static/js/2.a2200978.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.a2200978.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d5cd0f10645810cb389",
+    "url": "/portfolio/static/js/main.d8a5c281.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "d7dd55071e583814361f1c4adb5dcb86",
+    "url": "/portfolio/static/media/ai.d7dd5507.jpg"
+  },
+  {
+    "revision": "9eaeb173b2342b352bc97f7dcb0260ba",
+    "url": "/portfolio/static/media/atom.9eaeb173.png"
+  },
+  {
+    "revision": "f2c2597955578a744b8bcd0775ff48c8",
+    "url": "/portfolio/static/media/axios.f2c25979.png"
+  },
+  {
+    "revision": "cba8f09870b2b467217343760e9fe4bb",
+    "url": "/portfolio/static/media/background.cba8f098.png"
+  },
+  {
+    "revision": "f331c6270d8483f6a461ac8d2c4f3f6e",
+    "url": "/portfolio/static/media/button.f331c627.png"
+  },
+  {
+    "revision": "364e4ec720b7c5feda608d07d61dfc58",
+    "url": "/portfolio/static/media/calculator.364e4ec7.png"
+  },
+  {
+    "revision": "800b1c6b59ee3b085c9521ab22474e87",
+    "url": "/portfolio/static/media/clock.800b1c6b.png"
+  },
+  {
+    "revision": "c2c1a107fc098e11bb2ee71c0ca3714b",
+    "url": "/portfolio/static/media/css.c2c1a107.jpg"
+  },
+  {
+    "revision": "ecdf3ef2bb1a259660d3db770956cba7",
+    "url": "/portfolio/static/media/dice.ecdf3ef2.png"
+  },
+  {
+    "revision": "3cacc98a71fda4115686d17b9834250d",
+    "url": "/portfolio/static/media/es6.3cacc98a.jpeg"
+  },
+  {
+    "revision": "b5f24bb7979ed754fb9b042b38043e5d",
+    "url": "/portfolio/static/media/git_crop.b5f24bb7.png"
+  },
+  {
+    "revision": "2b53cb5422c9974fb00292901c2f6105",
+    "url": "/portfolio/static/media/googleClone.2b53cb54.png"
+  },
+  {
+    "revision": "763b1126a461040e1d730179342d6ab1",
+    "url": "/portfolio/static/media/html.763b1126.jpg"
+  },
+  {
+    "revision": "7aaf2195f15dc539f74bdfc6190c3166",
+    "url": "/portfolio/static/media/js.7aaf2195.png"
+  },
+  {
+    "revision": "cad04b1a8af6bb65a7444be36f167f6a",
+    "url": "/portfolio/static/media/mail1_crop.cad04b1a.png"
+  },
+  {
+    "revision": "424f5b5480bdd9923b20d356cebed05e",
+    "url": "/portfolio/static/media/marble_edit.424f5b54.png"
+  },
+  {
+    "revision": "f9cfa0b95e4faa6f2505a54dc9d9d7a1",
+    "url": "/portfolio/static/media/player.f9cfa0b9.png"
+  },
+  {
+    "revision": "fb8c441c652676c3a690474478dd177c",
+    "url": "/portfolio/static/media/postman.fb8c441c.jpeg"
+  },
+  {
+    "revision": "a3d4a589b7541663b3c398bfe2dc53fe",
+    "url": "/portfolio/static/media/profilePic.a3d4a589.jpg"
+  },
+  {
+    "revision": "11e90c29b7c1d54a7f1a300586dc7159",
+    "url": "/portfolio/static/media/react.11e90c29.png"
+  },
+  {
+    "revision": "6a0c3881f55442a62c94d25a05b8f6e0",
+    "url": "/portfolio/static/media/shoplane.6a0c3881.png"
+  },
+  {
+    "revision": "6d30693130e43e7882ea1ea552796189",
+    "url": "/portfolio/static/media/skills_crop.6d306931.png"
+  },
+  {
+    "revision": "992ad88faf34715a1d6eac32ae7a5f9d",
+    "url": "/portfolio/static/media/smartwatch.992ad88f.png"
+  },
+  {
+    "revision": "d8bde2f87f78fb152278e792b58e6d4c",
+    "url": "/portfolio/static/media/todo.d8bde2f8.png"
+  },
+  {
+    "revision": "d6618b1ff566e25442d42c221b82d19f",
+    "url": "/portfolio/static/media/vegan.d6618b1f.png"
+  },
+  {
+    "revision": "6344f720438781f89f46c2889f17dd74",
+    "url": "/portfolio/static/media/web.6344f720.png"
+  },
+  {
+    "revision": "f700e1e7d9a4922477f6aba624fc98d5",
+    "url": "/portfolio/static/media/web2.f700e1e7.png"
+  },
+  {
+    "revision": "3d92c4de7169f4137cc11c0fc4090772",
+    "url": "/portfolio/static/media/xammp2.3d92c4de.jpg"
+  }
+]);
