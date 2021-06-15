1da2c5af0ac20edafaeefa48d23a155b545eba97
GabrielNumaX <37051017+GabrielNumaX@users.noreply.github>
2021-06-14 21:08:53 -0300

    updated JUN 2021 user.email for git email

--- src/Components/Intro/Intro.js
+++ src/Components/Intro/Intro.js
@@ -4,7 +4,7 @@ import css from './Intro.module.css';
 
 const Intro = () => {
 
-    return(
+    return (
 
         <div className={css.IntroMain}>
 
@@ -15,15 +15,11 @@ const Intro = () => {
             <div className={css.DivIntro}>
                 <h2 className={css.IntroH2}>Hello I'm Gabriel</h2>
                 <p className={css.IntroP}>
-                    A Programmer graduated from Argentina's Universidad Tecnologica Nacional 
-                    with a Technical Degree in Programming.<br/>
-                    I have a handful of projects that you can check here and I am currently 
-                    working as MERN Stack Developer at <a className={css.ApploreLink} href="https://www.applore.in" target="_blank" rel="noopener noreferrer">Applore</a> (India). <br/>
-                    I look forward to keep gaining experience working in challenging projects and
-                     in near future become a Software Engineer.  <br/>
-                    I'm available for any kind of jobs that are within the reach of my expertise, 
-                    so feel free to send me a message through the Contact Form and I'll 
-                    be glad to answer you.
+                    A MERN Stack Seveloper with a college degree from Argentina's Universidad Tecnologica Nacional.
+                    <br/>
+                    I have more than two years of experience as a developer and I'm currently working at <a className={css.ApploreLink} href="https://beanstalkedu.com" target="_blank" rel="noopener noreferrer">Beanstalk</a> (India). <br />
+                    I'm looking forward to participate in large-scale projects that will allow me to apply my skills.
+                    <br/>Currently I'm available for any kind of projects that are within my field of expertise, so feel free to send me a message through the Contact Form and I'll be glad to answer you.
                 </p>
             </div>
         </div>
--- src/Components/Skills/Skills.js
+++ src/Components/Skills/Skills.js
@@ -6,28 +6,30 @@ import js from '../../assets/icons/js.png'
 import es6 from '../../assets/icons/es6.jpeg'
 import react from '../../assets/icons/react.png'
 import redux from '../../assets/icons/redux.png'
-import axios from '../../assets/icons/axios.png'
-import html from '../../assets/icons/html.jpg'
-import cssImg from '../../assets/icons/css.jpg'
+// import axios from '../../assets/icons/axios.png'
+// import html from '../../assets/icons/html.jpg'
+// import cssImg from '../../assets/icons/css.jpg'
 import sass from '../../assets/icons/sass.png'
 import node from '../../assets/icons/node.png'
 import express from '../../assets/icons/express.png'
 import mongo from '../../assets/icons/mongo.png'
 import visual from '../../assets/icons/visualStudio.png'
-import atom from '../../assets/icons/atom.png'
+// import atom from '../../assets/icons/atom.png'
 import postman from '../../assets/icons/postman.jpeg'
-import xammp from '../../assets/icons/xammp2.jpg'
+// import xammp from '../../assets/icons/xammp2.jpg'
 import git from '../../assets/icons/git_crop.png'
 import npm from '../../assets/icons/npm.png'
 import powershell from '../../assets/icons/powershell.jpg'
 import ai from '../../assets/icons/ai.jpg'
 import java from '../../assets/icons/java.png'
 import python from '../../assets/icons/python.png'
+import slack from '../../assets/icons/slack.png'
+import asana from '../../assets/icons/asana-square.png'
 
 
 const Skills = () => {
 
-    return(
+    return (
 
         <div className={css.SkillsMain}>
 
@@ -40,6 +42,22 @@ const Skills = () => {
                 <div className={css.UlContainer}>
                     <h3 className={css.SkillsH3}>Skills</h3>
                     <ul>
+                        <li>MongoDB
+                            <img src={mongo} alt='mongo'
+                                className={css.ImgLogo40}></img>
+                        </li>
+                        <li>Express
+                            <img src={express} alt='express'
+                                className={css.ImgLogo40}></img>
+                        </li>
+                        <li>React
+                            <img src={react} alt='react'
+                                className={css.ImgLogo40}></img>
+                        </li>
+                        <li>Node
+                            <img src={node} alt='node'
+                                className={css.ImgLogo40}></img>
+                        </li>
                         <li>Javascript
                             <img src={js} alt='javascript'
                                 className={css.ImgLogo40}></img>
@@ -48,42 +66,27 @@ const Skills = () => {
                             <img src={es6} alt='es6'
                                 className={css.ImgLogo40}></img>
                         </li>
-                        <li>ReactJS
-                            <img src={react} alt='react'
-                                className={css.ImgLogo40}></img>
+                        <li>Sass
+                            <img src={sass} alt='sass'
+                                className={css.ImgLogo50}></img>
                         </li>
+
                         <li>Redux
                             <img src={redux} alt='redux'
                                 className={css.ImgLogo40}></img>
                         </li>
-                        <li>Axios
+                        {/* <li>Axios
                             <img src={axios} alt='axios'
                                 className={css.ImgLogo50}></img>
-                        </li>
-                        <li>HTML
+                        </li> */}
+                        {/* <li>HTML
                             <img src={html} alt='html'
                                 className={css.ImgLogo40}></img>
                         </li>
                         <li>CSS
                             <img src={cssImg} alt='css'
                                 className={css.ImgLogo40}></img>
-                        </li>
-                        <li>Sass
-                            <img src={sass} alt='sass'
-                                className={css.ImgLogo40}></img>
-                        </li>
-                        <li>NodeJs
-                            <img src={node} alt='node'
-                                className={css.ImgLogo40}></img>
-                        </li>
-                        <li>Express
-                            <img src={express} alt='express'
-                                className={css.ImgLogo40}></img>
-                        </li>
-                        <li>MongoDB
-                            <img src={mongo} alt='mongo'
-                                className={css.ImgLogo40}></img>
-                        </li>
+                        </li> */}
                     </ul>
                 </div>
             </div>
@@ -96,10 +99,10 @@ const Skills = () => {
                             <img src={visual} alt='vs code'
                                 className={css.ImgLogo50}></img>
                         </li>
-                        <li>Atom
+                        {/* <li>Atom
                             <img src={atom} alt='atom'
                                 className={css.ImgLogo50}></img>
-                        </li>
+                        </li> */}
                         <li>Postman
                             <img src={postman} alt='postman'
                                 className={css.ImgLogo50}></img>
@@ -112,12 +115,20 @@ const Skills = () => {
                             <img src={powershell} alt='powershell'
                                 className={css.ImgLogo50}></img>
                         </li>
-                        <li>Xammp 
+                        {/* <li>Xammp
                             <img src={xammp} alt='xammp'
                                 className={css.ImgLogo40}></img>
-                        </li>
+                        </li> */}
                         <li>Git
                             <img src={git} alt='git'
+                                className={css.ImgLogo40}></img>
+                        </li>
+                        <li>Slack
+                            <img src={slack} alt='slack'
+                                className={css.ImgLogo50}></img>
+                        </li>
+                        <li>Asana
+                            <img src={asana} alt='asana'
                                 className={css.ImgLogo40}></img>
                         </li>
                     </ul>
@@ -128,10 +139,10 @@ const Skills = () => {
                 <div className={css.UlContainer}>
                     <h3 className={css.SkillsH3}>Interests</h3>
                     <ul>
-                        <li>Java
+                        {/* <li>Java
                             <img src={java} alt='java'
                                 className={css.ImgLogo40}></img>
-                        </li>
+                        </li> */}
                         <li>Python
                             <img src={python} alt='python'
                                 className={css.ImgLogo50}></img>
